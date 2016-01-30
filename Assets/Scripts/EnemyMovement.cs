ff069831c80bd8d31b5a3284470063bad05ec320
Kamil Patena <kamilpatena1221@gmail.com>
2016-01-30 09:06:28 -0800

    Enemy animations

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+	}
+}
