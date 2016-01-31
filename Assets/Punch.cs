fab25f831411ead541efc32a38333c7f6780f746
Sam Salvail <ssalvail39@gmail.com>
2016-01-30 16:03:05 -0800

    Added prefab for player and enemy; blood stuff works

---
+++ Assets/Punch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Punch : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject bloodParticle;
+
+    public void die()
+    {
+        Instantiate(bloodParticle, enemy.transform.position, enemy.transform.rotation);
+        Destroy(enemy);
+    }
+}
